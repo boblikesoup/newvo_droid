395d47fa6f3fa8a2236f574b55b236ddd343a1ba
redwraith <davidjaenisch@gmail.com>
2014-04-07 21:23:30 -0700

    Add pictures to post request

--- NewVo/src/main/java/com/newvo/android/request/CreatePostRequest.java
+++ NewVo/src/main/java/com/newvo/android/request/CreatePostRequest.java
@@ -1,6 +1,9 @@
 package com.newvo.android.request;
 
 import android.util.Log;
+import com.koushikdutta.ion.builder.Builders;
+
+import java.io.File;
 
 /**
  * Created by David on 4/6/2014.
@@ -10,6 +13,9 @@ public class CreatePostRequest extends PostRequest {
     public static final int PUBLIC = 0;
     public static final int FRIENDS = 1;
     public static final int GROUP = 2;
+
+    private String image1Location;
+    private String image2Location;
 
     CreatePostRequest() {
         super("/api/v1/posts");
@@ -25,6 +31,24 @@ public class CreatePostRequest extends PostRequest {
             return;
         }
         addBodyParam("viewable_by", viewableBy + "");
+    }
 
+    public void setImage1(String location){
+        this.image1Location = location;
+    }
+
+    public void setImage2(String location){
+        this.image2Location = location;
+    }
+
+    @Override
+    void addMiscData(Builders.Any.B load) {
+        if(image1Location != null){
+            load.setMultipartFile("image1", new File(image1Location));
+        }
+        if(image2Location != null){
+            load.setMultipartFile("image2", new File(image2Location));
+        }
+        super.addMiscData(load);
     }
 }
