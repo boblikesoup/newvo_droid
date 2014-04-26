8ad9f3c9f6e90086529326bfbd1782a5b93c39f7
davidjaenisch <davidjaenisch@gmail.com>
2014-04-26 09:35:10 -0700

    create CurrentUserProfileRequest

--- NewVo/src/main/java/com/newvo/android/remote/CurrentUserProfileRequest.java
+++ NewVo/src/main/java/com/newvo/android/remote/CurrentUserProfileRequest.java
@@ -1,11 +1,27 @@
 package com.newvo.android.remote;
 
+import com.newvo.android.parse.Post;
+import com.newvo.android.parse.User;
+import com.parse.FindCallback;
+import com.parse.ParseQuery;
+
 /**
  * Created by David on 4/26/2014.
  */
 public class CurrentUserProfileRequest {
 
-    public CurrentUserProfileRequest(){
+    private ParseQuery<Post> query;
 
+    public CurrentUserProfileRequest(){
+        User user = (User) User.getCurrentUser();
+
+        query = ParseQuery.getQuery(Post.class);
+        query.whereEqualTo("user_id", user);
     }
+
+    public void request(FindCallback<Post> callback){
+        query.findInBackground(callback);
+    }
+
+
 }
