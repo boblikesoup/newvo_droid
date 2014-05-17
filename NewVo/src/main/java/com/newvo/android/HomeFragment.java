83ae41c9dc9ce49b081e324b69f87e2eec88fee9
davidjaenisch <davidjaenisch@hotmail.com>
2014-05-17 12:38:00 -0700

    add loading text for homepage add "You have voted on all of the posts!" text for homepage

--- NewVo/src/main/java/com/newvo/android/HomeFragment.java
+++ NewVo/src/main/java/com/newvo/android/HomeFragment.java
@@ -1,10 +1,12 @@
 package com.newvo.android;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import butterknife.ButterKnife;
 import com.newvo.android.parse.Post;
 import com.newvo.android.remote.FeedRequest;
@@ -23,6 +25,8 @@ public class HomeFragment extends Fragment {
 
     private ComparisonViewHolder holder;
 
+    private boolean loadingPosts = true;
+
     public HomeFragment() {
         requestMorePosts();
     }
@@ -30,65 +34,99 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.comparison, container, false);
-        ButterKnife.inject(this, rootView);
-        if(holder == null){
-            holder = new ComparisonViewHolder(rootView, this);
-
-        } else {
-            holder.setView(rootView);
-        }
         if (posts != null) {
+            View rootView = inflater.inflate(R.layout.comparison, container, false);
+            ButterKnife.inject(this, rootView);
+            if (holder == null) {
+                holder = new ComparisonViewHolder(rootView, this);
+            } else {
+                holder.setView(rootView);
+            }
             if ((holder.getLastVotedPost() != null)) {
                 loadNextPost();
             } else {
                 loadPost();
             }
+            if(posts.size() != 0) {
+                return rootView;
+            }
+        }
+        View rootView = inflater.inflate(R.layout.text, container, false);
+        final TextView text = (TextView) rootView.findViewById(R.id.text);
+        if(!loadingPosts){
+            text.setText("You wizard! You have voted on every post!");
+            rootView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    loadingPosts = true;
+                    text.setText("Loading...");
+                    requestMorePosts();
+                }
+            });
         }
         return rootView;
     }
 
-    private void loadNextPost(){
-        if(posts.size() > 0 && holder != null && holder.getLastVotedPost() != null) {
+    private void loadNextPost() {
+        if (posts.size() > 0 && holder != null && holder.getLastVotedPost() != null) {
             posts.remove(holder.getLastVotedPost());
         }
-        if(posts.size() < 5){
+        if (posts.size() < 5) {
             requestMorePosts();
         }
         loadPost();
     }
 
-    private void loadPost(){
-        if(!posts.isEmpty()){
-                holder.setItem(posts.first());
+    private void loadPost() {
+        if (!posts.isEmpty()) {
+            holder.setItem(posts.first());
         } else {
             //TODO: You have read all of the posts, you wizard.
         }
     }
 
-    private void requestMorePosts(){
+    private void requestMorePosts() {
         new FeedRequest().request(new FindCallback<Post>() {
             @Override
             public void done(List<Post> posts, ParseException e) {
-                //Remove already voted on posts.
-                if(posts != null){
-                    if(holder != null){
-                        int size = posts.size();
-                        for(Post post : holder.getVotedPosts()){
-                            if(posts.contains(post)){
-                                posts.remove(post);
+                if (e == null) {
+                    //Remove already voted on posts.
+                    if (posts != null) {
+                        if (holder != null) {
+                            int size = posts.size();
+                            for (Post post : holder.getVotedPosts()) {
+                                if (posts.contains(post)) {
+                                    posts.remove(post);
+                                }
+                            }
+                            if (posts.size() == 0) {
+                                loadingPosts = false;
+                                if (size == FeedRequest.NUMBER_OF_POSTS) {
+                                    requestMorePosts();
+                                } else {
+                                    Activity activity = getActivity();
+                                    if (activity != null) {
+                                        ((DrawerActivity)getActivity()).attachDetachFragment();
+                                    }
+                                }
+                                return;
+                            }
+                            HomeFragment.this.posts.addAll(posts);
+                            if (holder.getPost() == null) {
+                                loadPost();
                             }
                         }
-                        if(posts.size() == 0 && size == FeedRequest.NUMBER_OF_POSTS){
-                            requestMorePosts();
+                    }
+                    if(loadingPosts){
+                        loadingPosts = false;
+                        Activity activity = getActivity();
+                        if (activity != null) {
+                            ((DrawerActivity)getActivity()).attachDetachFragment();
                         }
                     }
-                    HomeFragment.this.posts.addAll(posts);
-                    if(holder != null && holder.getPost() == null){
-                        loadPost();
-                    }
+                } else {
+                    requestMorePosts();
                 }
-
             }
         });
     }
