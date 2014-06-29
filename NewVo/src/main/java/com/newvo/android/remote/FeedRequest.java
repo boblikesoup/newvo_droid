2680d2d2efbdbb2b0ec2b0a0113b8f3e1e96fda0
davidjaenisch <davidjaenisch@hotmail.com>
2014-06-29 11:08:50 -0700

    combined feed request

--- NewVo/src/main/java/com/newvo/android/remote/FeedRequest.java
+++ NewVo/src/main/java/com/newvo/android/remote/FeedRequest.java
@@ -17,12 +17,17 @@ public class FeedRequest {
 
     public FeedRequest() {
         User user = User.getCurrentUser();
-
-        query = ParseQuery.getQuery(Post.class);
-        query.setLimit(NUMBER_OF_POSTS);
-        query.whereEqualTo(Post.STATUS, 0);
-        query.whereEqualTo(Post.VIEWABLE_BY, 0);
         String userId = user.getUserId();
+
+        ParseQuery<Post> taggedPosts = ParseQuery.getQuery(Post.class);
+        ParseQuery<Post> regularPosts = ParseQuery.getQuery(Post.class);
+        taggedPosts.whereContainedIn(Post.USER_TAG, Arrays.asList(userId));
+        regularPosts.whereEqualTo(Post.VIEWABLE_BY, 0);
+
+
+        query = ParseQuery.or(Arrays.asList(taggedPosts, regularPosts));
+
+        query.setLimit(NUMBER_OF_POSTS);
         query.whereNotEqualTo(Post.USER_ID, user);
         query.whereNotEqualTo(Post.STATUS, Post.getStatusValue(Post.DELETED));
         query.whereNotContainedIn(Post.VOTED_ON_ARRAY, Arrays.asList(userId));
